49cae9b19370b9728629adc414e0af194995c673
Piyush Verma <piyush.verma@my.jcu.edu.au>
2023-09-25 22:28:25 +1000

    Add function to score to determine result based on user input

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,13 +3,21 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    determine_result(score)
+
+
+def determine_result(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+main()
