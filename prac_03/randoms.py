f755f83de07adf1adc02c573ceb72df1ebb56193
Piyush Verma <piyush.verma@my.jcu.edu.au>
2023-09-27 20:45:07 +1000

    Answer smallest and largest number using different random methods as well as produce a random number between 1-100 inclusive.

---
+++ prac_03/randoms.py
@@ -0,0 +1,14 @@
+# Line 1
+# The smallest number I could have seen was 5 and the largest was 30.
+
+# Line 2
+# The smallest number I could have seen was 3 and largest was 9.
+# Line 2 could not have produced a 4.
+
+# Line 3
+# The smallest number I could have seen was 2.5 and largest was 5.5
+
+import random
+
+random_number = random.randint(1, 100)
+print(random_number)
