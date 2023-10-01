3aac792c7c4f2ab3f112cdd29381dd004d9fe7ee
Piyush Verma <piyush.verma@my.jcu.edu.au>
2023-10-01 19:32:10 +1000

    Do question 2 for files.py

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,6 +1,13 @@
 # 1. Write code that asks the user for their name, then opens a file called "name.txt" and writes that name to it.
 # Remember to close your file.
+# name = input("Name: ")
+# with open("name.txt", "w") as out_file:
+#     print(name, file=out_file)
 
-name = input("Name: ")
-with open("name.txt", "w") as out_file:
-    print(name, out_file)
+# 2 (In the same file, but as if it were a separate program) Write code that opens "name.txt" and reads the name (as
+# above) then prints, "Your name is Bob" (or whatever the name is in the file).
+# with open("name.txt", "r") as in_file:
+#     for line in in_file:
+#         print(f"Your name is {line}")
+
+
