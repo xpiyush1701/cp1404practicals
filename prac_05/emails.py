3ff8144306c54a6280fc902a91e8e3743f0c42ae
piyushverma <piyush.verma@my.jcu.edu.au>
2023-10-15 22:14:58 +1000

    Add actual time and reformat code.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,6 +1,6 @@
 """
 Estimate: 20 Minutes
-Actual:
+Actual: 14 Minutes
 """
 
 
@@ -19,11 +19,13 @@ def main():
     for name, email in name_to_email.items():
         print(f"{name} ({email})")
 
+
 def get_name_from_email(email):
     """Estimate and store name from emails"""
     prefix = email.split('@')[0]
     parts = prefix.split(".")
     name = " ".join(parts).title()
     return name
+
 
 main()
