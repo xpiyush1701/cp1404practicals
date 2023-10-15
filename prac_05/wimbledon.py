c320bc5323b840da5babdeb2de7b1ff32898adac
piyushverma <piyush.verma@my.jcu.edu.au>
2023-10-15 22:30:37 +1000

    Create appropriate functions (4).

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,3 +2,24 @@
 Estimate: 30 Minutes
 Actual:
 """
+
+def main():
+    data = get_sorted_data()
+    champion_to_count = process_records()
+    countries = process_data()
+    print_data()
+
+
+def get_sorted_data():
+    pass
+    return data
+
+def process_data():
+    pass
+    return data
+
+def print_data():
+    pass
+
+
+main()
