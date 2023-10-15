0a36b2b531af615df87e4ea7d6609a414ed66f11
piyushverma <piyush.verma@my.jcu.edu.au>
2023-10-15 22:14:23 +1000

    Fix all small errors and get dictionary to print out line by line using for loop.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -10,8 +10,14 @@ def main():
     email = input("Enter email address: ")
     while email != "":
         name = get_name_from_email(email)
-        print(name)
+        confirmation = input(f"Is your name {name}? (Y/n) ")
+        if confirmation.upper() != "Y" and confirmation != "":
+            name = input("Name: ")
+        name_to_email[email] = name
+        email = input("Email: ")
 
+    for name, email in name_to_email.items():
+        print(f"{name} ({email})")
 
 def get_name_from_email(email):
     """Estimate and store name from emails"""
