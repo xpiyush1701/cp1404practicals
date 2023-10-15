ec8d78e6259c5cc165decdc25760a0a04f9faf1a
piyushverma <piyush.verma@my.jcu.edu.au>
2023-10-15 21:56:36 +1000

    Add actual time taken to complete and docstring of failing to add sorted feature.

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,6 +1,6 @@
 """
 Estimate: 15 Minutes
-Actual:
+Actual: 7 Minutes
 """
 
 word_to_count = {}
@@ -10,6 +10,8 @@ words = text.split()
 for word in words:
     number_of_words = word_to_count.get(word, 0)
     word_to_count[word] = number_of_words + 1
+
+# Could not get sorted feature to work.
 
 max_length = max((len(word) for word in words))
 for word in words:
