db3196f4fab8117b5257c27fca44d5bbebe303bf
piyushverma <piyush.verma@my.jcu.edu.au>
2023-10-15 22:33:22 +1000

    Get data in nested lists form.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,20 +3,31 @@ Estimate: 30 Minutes
 Actual:
 """
 
+FILENAME = "wimbledon.csv"
+
+
 def main():
-    data = get_sorted_data()
+    data = get_sorted_data(FILENAME)
     champion_to_count = process_records()
     countries = process_data()
     print_data()
 
 
-def get_sorted_data():
-    pass
+def get_sorted_data(filename):
+    """Get data in nested lists form."""
+    data = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # Remove header
+        for line in in_file:
+            datum = line.strip().split(",")
+            data.append(datum)
     return data
+
 
 def process_data():
     pass
     return data
+
 
 def print_data():
     pass
