4f57c98084ae67bf17099a755d5d6d934252df4b
piyushverma <piyush.verma@my.jcu.edu.au>
2023-10-15 21:53:45 +1000

    Print count of each word.

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -11,3 +11,6 @@ for word in words:
     number_of_words = word_to_count.get(word, 0)
     word_to_count[word] = number_of_words + 1
 
+max_length = max((len(word) for word in words))
+for word in words:
+    print(f"{word:{max_length}} : {word_to_count[word]}")
