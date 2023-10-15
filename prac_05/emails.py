2d6cadf5c6b59e228aab59d29154b12a252a0e41
piyushverma <piyush.verma@my.jcu.edu.au>
2023-10-15 22:03:51 +1000

    Get email and create function that finds name through email.

---
+++ prac_05/emails.py
@@ -0,0 +1,19 @@
+"""
+Estimate: 20 Minutes
+Actual:
+"""
+
+
+def main():
+    """Main function"""
+    name_to_email = {}
+    email = input("Enter email address: ")
+    while email != "":
+        name = get_name_from_email(email)
+
+
+def get_name_from_email(email):
+    """Estimate and store name from emails"""
+
+
+main()
