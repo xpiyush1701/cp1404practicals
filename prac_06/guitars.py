b9e49d1af500ce3c8eb488598689c7ed5877f69b
Piyush Verma <piyush.verma@my.jcu.edu.au>
2023-10-31 21:40:09 +1000

    Add variables and a list to store.

---
+++ prac_06/guitars.py
@@ -0,0 +1,19 @@
+"""
+Estimated Time: 35 Minutes
+Actual Time: 21 Minutes
+"""
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    print("My guitars!")
+    guitars = []
+    guitar_number = 1
+
+    name = input("Name: ")
+    year = int(input("Year: "))
+    cost = float(input("Cost: "))
+
+
+main()
