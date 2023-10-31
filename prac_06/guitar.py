bacb2166c0209c86ff92e2aa517931ac82ae9626
Piyush Verma <piyush.verma@my.jcu.edu.au>
2023-10-31 21:21:06 +1000

    Return guitar age using get_age, and find out if the guitar is vintage by returning True or False.

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -5,10 +5,16 @@ Actual Time:
 
 
 class Guitar:
-    def __init__(self, name, year, cost):
+    def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
-        return f"{self.name} ({self.year}) - ${self.cost:.2f}"
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
+
+    def get_age(self, present_year):
+        return present_year - self.year
+
+    def is_vintage(self, present_year):
+        return self.get_age(present_year) >= 50
