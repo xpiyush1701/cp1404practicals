ef50f0c3dd9b444fe9e237c475dbc961f0adf422
Piyush Verma <piyush.verma@my.jcu.edu.au>
2023-10-31 21:25:31 +1000

    Create objects and define current year.

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,11 @@
+"""
+Estimated Time: 35 Minutes
+Actual Time:
+"""
+
+from prac_06.guitar import Guitar
+
+g1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+g2 = Guitar("Another Guitar", 2013)
+
+current_year = 2023
