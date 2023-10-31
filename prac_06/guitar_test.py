6d8938f0d8a30626df8c0e7a40154bc95f10c858
Piyush Verma <piyush.verma@my.jcu.edu.au>
2023-10-31 21:33:36 +1000

    Complete and test all 'test runs'

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -8,4 +8,9 @@ from prac_06.guitar import Guitar
 g1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
 g2 = Guitar("Another Guitar", 2013)
 
-current_year = 2023
+present_year = 2023
+
+print(f"{g1.name} get_age() - Expected 100. Got {g1.get_age(present_year)}")
+print(f"{g2.name} get_age() - Expected 9. Got {g2.get_age(present_year)}")
+print(f"{g1.name} is_vintage() - Expected True. Got {g1.is_vintage(present_year)}")
+print(f"{g2.name} is_vintage() - Expected False. Got {g2.is_vintage(present_year)}")
