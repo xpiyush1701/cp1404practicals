7fd1f300ae898b8dc8c41383bfb2fa4fdff463f7
Piyush Verma <piyush.verma@my.jcu.edu.au>
2023-10-31 20:53:08 +1000

    Add init, return dynamic True or False and create attributes.

---
+++ prac_06/languages.py
@@ -0,0 +1,15 @@
+"""
+Estimated Time: 15 Minutes
+Actual Time:
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+
+main()
---
+++ prac_06/programming_language.py
@@ -0,0 +1,16 @@
+"""
+Estimated Time: 15 Minutes
+Actual Time:
+"""
+
+
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+
