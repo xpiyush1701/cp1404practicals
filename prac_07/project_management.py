dcfca222863811765787fc0f0aa882e721d7dc7a
piyushverma <piyush.verma@my.jcu.edu.au>
2023-11-05 20:11:15 +1000

    Load and save project_management.py first as they are easy to do with the provided resource and existing knowledge.

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -10,11 +10,33 @@ from datetime import datetime
 class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         self.name = name
-        self.start_date = start_date
-        self.priority = priority
-        self.cost_estimate = cost_estimate
-        self.completion_percentage = completion_percentage
+        self.start_date = datetime.strptime(start_date, "%d/%m/%Y")
+        self.priority = int(priority)
+        self.cost_estimate = float(cost_estimate)
+        self.completion_percentage = int(completion_percentage)
 
     def __str__(self):
         return f"{self.name} (Start Date: {self.start_date.strftime('%d/%m/%Y')}, Priority: {self.priority}, " \
                f"Cost Estimate: {self.cost_estimate}, Completion Percentage: {self.completion_percentage}%)"
+
+
+def load_projects(filename, projects):
+    projects = []
+    with open(filename, "r") as in_file:
+        reader = csv.DictReader(in_file)
+        for row in reader:
+            project = Project(row['Name'], row['Start Date'], row['Priority'], row['Cost Estimate'],
+                              row['Completion Percentage'])
+            projects.append(project)
+        return projects
+
+
+def save_projects(filename, projects):
+    with open(filename, 'w', newline='') as file:
+        writer = csv.DictWriter(file,
+                                fieldnames=['Name', 'Start Date', 'Priority', 'Cost Estimate', 'Completion Percentage'])
+        writer.writeheader()
+        for project in projects:
+            writer.writerow({'Name': project.name, 'Start Date': project.start_date.strftime('%d/%m/%Y'),
+                             'Priority': project.priority, 'Cost Estimate': project.cost_estimate,
+                             'Completion Percentage': project.completion_percentage})
